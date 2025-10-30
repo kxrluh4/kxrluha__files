efd166aba21adaadd2f03c73826b90d028ca196b
kxrluh4 <karliskuzmans07lol@gmail.com>
2025-10-30 15:23:10 +0200

    atjauninajums

--- karliskuzmans_OOP.py
+++ karliskuzmans_OOP.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 logs=tk.Tk()
-logs.geometry("600x550")
+logs.geometry("1000x800")
 logs.title("spiediena aprekins")
-logs.background="#AC97CC"
+logs.configure(bg="lightblue")
 #izveidoju logu spiediena aprekins
 
 def aprekinat_spiedienu():# aprēķina spiedienu pēc formulas p=F/S
@@ -12,22 +12,22 @@ def aprekinat_spiedienu():# aprēķina spiedienu pēc formulas p=F/S
         teksts2=ievade_laukums.get()
 
         F=speks=int(teksts1)
-        S=laukums=int(teksts2)
-        p=spiediens=F/S
+        A=laukums=int(teksts2)
+        p=spiediens=F/A
         rezultats.config(text=f"spiediens ir: {p} Pa")
-        if S==0:
-            messagebox.showerror("Laukums nedrīkst būt nulle")
-        else:
+        if A==0:
+            messagebox.showerror("kluda", "laukums nevar but nulle!!!")
+        else:#ja laukums nav nulle, aprēķina spiedienu
             p=F/S
             rezultats.config(text=f"spiediens ir: {p} Pa")
-    except ValueError:
-        messagebox.showwarning("kluda","Ievadiet derīgus skaitļus")
+    except ValueError:#pārbauda vai ievadītās vērtības ir skaitliskas
+        messagebox.showerror("Kluda", "ievadi derigus skaitlus!")
 
 
 tk.Label(logs, text="ievadi spēku (F):").pack(pady=10)
 ievade_speks=tk.Entry(logs)
 ievade_speks.pack()
-tk.Label(logs, text="ievadi laukumu (S):").pack(pady=10)
+tk.Label(logs, text="ievadi laukumu (A):").pack(pady=10)
 ievade_speks=tk.Entry(logs)
 ievade_speks.pack()
 tk.Label(logs, text="ievadi spiedienu (p):").pack(pady=10)
@@ -39,13 +39,9 @@ poga.pack(pady=10)
 rezultats=tk.Label(logs, text="spiediens ir: ")
 rezultats.pack(pady=10)
 
-
-
-
-
-
-
-
+attels=tk.PhotoImage(file="spiediens2.png")
+etikets_attels=tk.Label(logs, image=attels, bg="lightblue")
+etikets_attels.pack(pady=10)
 
 
 
